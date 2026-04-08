78931952b5364eef6f850410280ad34bc72377a1
YogyerekDEV <mszebi2008@gmail.com>
2026-04-08 14:02:29 +0200

    dev(W-01): add swaggerUI

--- Webshop.api/Program.cs
+++ Webshop.api/Program.cs
@@ -3,7 +3,16 @@ using Webshop.api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapAuthEndpoints();
 app.MapProductsEndpoints();
