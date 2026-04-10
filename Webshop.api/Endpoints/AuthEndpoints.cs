39725a641157c64edf50e36e71d552616584787b
YogyerekDEV <mszebi2008@gmail.com>
2026-04-10 13:06:46 +0200

    fix(W-01): add authorization step to logout

--- Webshop.api/Endpoints/AuthEndpoints.cs
+++ Webshop.api/Endpoints/AuthEndpoints.cs
@@ -15,6 +15,6 @@ public static class AuthEndpoints
         app.MapPost("/auth/request-update", async (AuthService authService) => await authService.RequestUpdateCode()).RequireAuthorization();
         app.MapPut("/auth/update", async (UpdateUserDto dto, AuthService authService) => await authService.Update(dto)).RequireAuthorization();
         app.MapPost("/auth/me", async (AuthService authService) => await authService.Me()).RequireAuthorization();
-        app.MapGet("/auth/logout", (AuthService authService) => authService.Logout());
+        app.MapGet("/auth/logout", (AuthService authService) => authService.Logout()).RequireAuthorization();
     }
 }
